51145c64732ac1074e22aacd63d3c780903106a7
Dusan Stevic <stevicdule@gmail.com>
2020-06-12 20:14:33 +0200

    rating system

--- backend/src/main/java/rs/ac/uns/ftn/backend/controller/RecipeController.java
+++ backend/src/main/java/rs/ac/uns/ftn/backend/controller/RecipeController.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import rs.ac.uns.ftn.backend.exceptions.BadRequestException;
+import rs.ac.uns.ftn.backend.exceptions.ResourceNotFoundException;
 import rs.ac.uns.ftn.backend.model.Recipe;
 import rs.ac.uns.ftn.backend.service.RecipeService;
 
@@ -30,5 +38,35 @@ public class RecipeController {
 		
 		return new ResponseEntity<>(recipes, HttpStatus.OK);
 	}
+	
+	// TO DO PREDJI NA DTO
+	/* get an recipe by id, permitted for all */
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Recipe> getRecipeById(@PathVariable(value = "id") Long recipeId) throws ResourceNotFoundException {
+		Recipe recipe = recipeService.getRecipeById(recipeId);
+		return new ResponseEntity<>(recipe, HttpStatus.OK);
+	}
+	
+	
+	/*
+	 * @DeleteMapping("/cancel/{id}")
+	 * 
+	 * @PreAuthorize("hasRole('ROLE_USER')") public ResponseEntity
+	 * cancelTicket(@PathVariable Long id) { ticketsService.cancelTicket(id); return
+	 * ResponseEntity.ok().build(); }
+	 */
+	
+
+	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Recipe> addRecipeGrade(@PathVariable(value = "id") Long recipeId,@RequestParam(value = "grade") int grade)
+			throws ResourceNotFoundException,BadRequestException{
+		Recipe recipe = recipeService.addRecipeGrade(recipeId, grade);
+		return new ResponseEntity<>(recipe, HttpStatus.OK);
+		
+	}
+	
+
 
 }
--- backend/src/main/java/rs/ac/uns/ftn/backend/service/RecipeService.java
+++ backend/src/main/java/rs/ac/uns/ftn/backend/service/RecipeService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import rs.ac.uns.ftn.backend.exceptions.BadRequestException;
+import rs.ac.uns.ftn.backend.exceptions.ResourceNotFoundException;
 import rs.ac.uns.ftn.backend.model.Recipe;
 import rs.ac.uns.ftn.backend.repository.RecipeRepository;
 
@@ -15,6 +17,46 @@ public class RecipeService {
 	public List<Recipe> findAll() {
 		return recipeRepository.findAll();
 	}
+	
+	public Recipe getRecipeById(Long id) throws ResourceNotFoundException {
+		Recipe  recipe  = recipeRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recipe with id '" + id + "' doesn't exist."));
+
+		return recipe;
+	}
+	
+   
+    
+	public Recipe addRecipeGrade(Long id, int grade) throws ResourceNotFoundException,BadRequestException {
+		Recipe  recipe  = recipeRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recipe with id '" + id + "' doesn't exist."));
+        if(!(1 <= grade && grade <= 5)){
+            throw new BadRequestException("Recipe grade not in range from 1 to 5");
+        }
+		switch (grade) {
+		  case 1:
+		    recipe.getRating().setFrequency1(recipe.getRating().getFrequency1()+1);
+		    break;
+		  case 2:
+		    recipe.getRating().setFrequency2(recipe.getRating().getFrequency2()+1);
+		    break;
+		  case 3:
+		    recipe.getRating().setFrequency3(recipe.getRating().getFrequency3()+1);
+		    break;
+		  case 4:
+		    recipe.getRating().setFrequency4(recipe.getRating().getFrequency4()+1);
+		    break;
+		  case 5:
+			recipe.getRating().setFrequency5(recipe.getRating().getFrequency5()+1);
+		    break;
+		  default:
+		    System.out.println("Recipe grade not in range from 1 to 5");
+		}
+		recipeRepository.save(recipe);
+		return recipe;
+		
+	}
+
 	
 
 }
